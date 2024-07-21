870d93eb9675d0bc674bc4514359af2fc343182f
MAN MOHIT <97469161+GM-897@users.noreply.github.com>
2024-07-21 21:20:00 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,7 +2,7 @@ import space from './space-con.png';
 import gm from '../images/HeroImage.jpg'
 import paras from '../images/paras.png'
 import netflix from '../images/Netflix.png'
-import nudge from '../images/Nudge.png'
+import nudge from '../images/Nudge .png'
 import note from '../images/myNote.png'
 
 export const Bio = {
